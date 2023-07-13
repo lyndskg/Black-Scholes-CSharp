4174feec252b6c8d55b0af0e251413abef4a1111
lyndskg <lyndskg@umich.edu>
2023-07-13 00:51:02 -0500

    Implemented constructors, member variables, and option price calculator based on Abramowitz & Stegun's (1964) approximation of normalCDF.
    Current optimization dilemmas/tradeoffs:
    - put/call option: enum w/ switch case or char w/ if-else?
    - finding potentially more efficient normCDF approximations
    - further segmentation of CalculateOptionPrice() function
    
    Next:
    - Include The Greeks, Implied Volatility, Heston Model, etc.
    - VALIDATE, TEST & OPTIMIZE!

--- BlackScholes/BlackScholesModel.cs
+++ BlackScholes/BlackScholesModel.cs
@@ -5,21 +5,36 @@ namespace BlackScholes
     public class BlackScholesModel
     {
         // Member variables
-        private double underlyingPrice;
+        private double underlyingPrice; 
         private double strikePrice;
         private double timeToExpiration;
         private double riskFreeRate;
         private double volatility;
-
-
-        // Constructor
-        public BlackScholesModel()
+        
+        // private char callPutFlag; // Determine if it is a call or put order.
+        private enum OptionContractType
         {
-            // Default constructor
-            // Initialize member variables if needed
+            Call = 1,
+            Put = 2
         }
 
-        // Option pricing function
+        // Default constructor
+        public BlackScholesModel()
+        {
+        }
+
+        // Custom constructor initializing all member variables (Is this necessary??)
+        public BlackScholesModel(double underlyingPrice, double strikePrice, double timeToExpiration,
+            double riskFreeRate, double volatility, OptionContractType contractType)
+        {
+            this.UnderlyingPrice = underlyingPrice;
+            this.StrikePrice = strikePrice;
+            this.TimeToExpiration = timeToExpiration;
+            this.Volatility = volatility;
+            this.ContractType = contractType;
+        }
+        
+        // Calculates theoretical option price.
         public double CalculateOptionPrice()
         {
             // Calculate the intermediate variables.
@@ -28,12 +43,74 @@ namespace BlackScholes
                         (volatility * Math.Sqrt(timeToExpiration));
             double d2 = d1 - volatility * Math.Sqrt(timeToExpiration);
 
-            // Calculate the option price using the Black-Scholes formula.
-            double optionPrice = underlyingPrice * NormalCDF(d1) -
-                                 strikePrice * Math.Exp(-riskFreeRate * timeToExpiration) * NormalCDF(d2);
+            // double optionPrice = 0.0;
 
-            return optionPrice;
-        }
+            // Calculate the option price using the Black-Scholes formula.
+            switch (OptionContractType)
+            {
+                case OptionContractType.Call:
+                    return underlyingPrice * NormalCDF(d1) -
+                           strikePrice * Math.Exp(-riskFreeRate * timeToExpiration) * NormalCDF(d2);
+                case OptionContractType.Put:
+                    return strikePrice * Math.Exp(-riskFreeRate * timeToExpiration) * NormalCDF(-d2) -
+                           underlyingPrice * NormalCDF(-d1);
+                default:
+                    Console.WriteLine("Error: Option type does not exist!");
+                    return -1;
+                    //throw new NotSupportedException("Error: Order type does not exist!");
+            }
+        
+            // if (flag == "c") // If it is a call option. 
+            // {
+            //     optionPrice = underlyingPrice * NormalCDF(d1) -
+            //                   strikePrice * Math.Exp(-riskFreeRate * timeToExpiration) * NormalCDF(d2);
+            // }
+            // else if (flag == "p") // Else, if it is a put option.
+            // {
+            // optionPrice = strikePrice * Math.Exp(-riskFreeRate * timeToExpiration) * NormalCDF(-d2) -
+            //               underlyingPrice * NormalCDF(-d1);
+            // }
+            //
+            // return optionPrice;
+            
+            
+        } // CalculateOptionPrice()
+
+        // Approximate the CDF of a normal distribution. 
+        public double NormalCDF(double d)
+        {
+            // Abramowitz & Stegun's (1964) approximation. 
+            
+            /* Refer to https://www.ijser.org/researchpaper/Approximations-to-Standard-Normal-Distribution-Function.pdf
+               for potentially more efficient approximations wrt M.A.E. and corresponding z-score */
+            
+            double result = 0.0;
+
+            const double a[] =
+            {
+                0.2316419, 0.31938153, -0.356563782, 1.781477937, -1.821255978, 1.330274429
+            };
+
+            double L, K = 0.0;
+            
+            L = Math.Abs(d);
+            K = 1.0 / (1.0 + a[0] * L);
+
+            result = 1.0 - 1.0 / Math.Sqrt(2 * Convert.ToDouble(Math.PI.ToString())) * Math.Exp(-L * L / 2.0) *
+                (a[1] * K + a[2] * a[3] * Math.Pow(K, 3.0) + a[4] * Math.Pow(K, 4.0) + a[5] * Math.Pow(K, 5.0));
+
+            return d < 0 ? 1.0 - result : result; // Should ternary operator be used here? Or if-else statement (below)?
+            
+            /* if (d < 0) 
+            {
+            return 1.0 - result;
+            }
+            else 
+            { 
+                return result;
+            } */
+            
+        } // NormalCDF()
 
         // Setter methods
         public void SetUnderlyingPrice(double price)
@@ -60,5 +137,6 @@ namespace BlackScholes
         {
             volatility = vol; // Volatility
         }
-    }
+        
+    } // BlackScholesModel 
 } 
--- BlackScholes/Main.cs
+++ BlackScholes/Main.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 namespace BlackScholes
 {
@@ -9,15 +9,6 @@ namespace BlackScholes
             // Instantiate and use the BlackScholes Model
             BlackScholesModel model = new BlackScholesModel();
             
-            // Set the necessary member variables using setting methods
-            /* e.g.
-                model.SetUnderlyingPrice(10.0);
-                model.SetStrikeprice(9.0);
-                model.SetTimeToExpiration(1.5);
-                model.SetRiskFreeRate(0.7);
-                model.SetVolatility(0.9); 
-             */
-
             // Call the option pricing function
             double optionPrice = model.CalculateOptionPrice();
 
